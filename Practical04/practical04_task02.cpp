cf5730951d92fd85f22275871856c5e08e751391
Keshav Naithani <66078936+KeshavNaithani@users.noreply.github.com>
2021-09-29 10:41:25 +0530

    Add files via upload

---
+++ Practical04/practical04_task02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "first.h"
+#include "second.h"
+using namespace std;
+
+int main(){
+        cout<<add(2,1)<<endl;
+        cout<<add(3.1,2.3)<<endl;
+        return 0;
+}
