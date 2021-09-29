6014319d52023a43b71dc08ec4e425de4481d7ae
Keshav Naithani <66078936+KeshavNaithani@users.noreply.github.com>
2021-09-29 10:25:43 +0530

    Create Practical04_task1.cpp

---
+++ Practical04/Practical04_task1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "first.h"
+#include "second.h"
+using namespace std;
+
+int main(){
+        cout<<First::add(2,1)<<endl;
+        cout<<Second::add(3.1,2.3)<<endl;
+        return 0;
+}
